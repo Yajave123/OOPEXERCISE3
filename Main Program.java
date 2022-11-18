c9bed421967177231705290f518722fcd4431791
Yajave123 <116291411+Yajave123@users.noreply.github.com>
2022-11-18 21:11:51 +0800

    Update Main Program.java

--- Main Program.java
+++ Main Program.java
@@ -1,18 +1,13 @@
-public class Main {
-	public static void main(String[] args) {
-		Database db = new Database("tmp", 20);
-		DataHandler dh = new DataHandler(db);
-		
-		dh.addData("John Wattz");
-		dh.addData("Sherlock Holmes");
-		dh.addData("Jenny Trevor");
-		
-		System.out.println(dh.getDatabaseName() + " " + db.getCurrentSize());
-		dh.viewAll();
-		
-		dh.setDatabaseName("Names");
-		
-		System.out.println("\n" + dh.getDatabaseName() + " " + db.getCurrentSize());
-		dh.viewAll();
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		iPad apple=new iPad("iPad Air");
+		iPad specs=new iPad("12MP Ultra Wide front camera");
+	
+		System.out.println(apple.getModel() + " " +  specs.getModel());
+
 	}
 }
